ed2ab3f8a777766b1bf9c86c19cc464516141656
Patrikkkk <kopcpatr@gmail.com>
2023-01-25 01:07:18 +0100

    add styles for login page/desktop

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,6 +2,7 @@
     // icons
     import stone1_src from '$lib/assets/icons/login/signup1.svg';
     import stone2_src from '$lib/assets/icons/login/signup2.svg';
+    import people_src from '$lib/assets/icons/login/people.svg';
 
     // types
     import type { IPageData } from '$lib/ts-interfaces';
@@ -40,36 +41,76 @@
     <WBack />
 </div>
 <div class="form-wrapper">
-    <div class="top">
+    <div class="form-wrapper__top">
         <InlineSVG src={stone1_src} />
     </div>
-    <div class="form">
+    <div class="form-wrapper__middle">
         <WSignup />
     </div>
-    <div class="bottom">
+    <div class="form-wrapper__bottom">
         <InlineSVG src={stone2_src} />
+    </div>
+</div>
+<div class="content-wrapper">
+    <span class="content-wrapper__small"> Story begins </span>
+    <h2 class="content-wrapper__title">Raise a Glass and Join the Craft Beer Revolution</h2>
+    <p class="content-wrapper__text">
+        Joining "Find-Brew" is a great way to connect with other beer enthusiasts and expand your knowledge of different
+        brews.
+    </p>
+    <div class="content-wrapper__icon">
+        <InlineSVG src={people_src} style="margin: 0 auto;" />
     </div>
 </div>
 
 <style lang="scss">
     .form-wrapper {
         position: relative;
-        .top {
+        &__top {
             position: absolute;
             top: -10%;
             right: 0;
             z-index: 0;
         }
-        .form {
+        &__middle {
             position: relative;
             margin-top: 70px;
             z-index: 1;
         }
-        .bottom {
+        &__bottom {
             position: absolute;
             bottom: -60px;
             left: 0;
             z-index: 0;
+        }
+    }
+    .content-wrapper {
+        background-color: var(--page);
+        text-align: center;
+        border-top: 2px solid var(--border);
+        max-width: 460px;
+        margin: 0 auto;
+        margin-top: 120px;
+        padding-top: 40px;
+
+        &__small {
+            font-size: 16px;
+            font-weight: 400;
+        }
+        &__title {
+            font-size: 26px;
+            line-height: 40px;
+            font-weight: 900;
+            margin-top: 16px;
+        }
+        &__text {
+            margin-top: 24px;
+            font-size: 20px;
+            font-weight: 300;
+            line-height: 34px;
+        }
+        &__icon {
+            padding: 60px 0 50px 0;
         }
     }
 </style>
